b55a044cfdc8f43028b2de9d2a1e41e641036c50
nigarmdva <nigar.m@div.edu.az>
2025-04-11 23:40:01 +0400

    m

--- src/components/RecentHouses.jsx
+++ src/components/RecentHouses.jsx
@@ -6,6 +6,8 @@ import houseData from '../data/house'
 import { CiLocationOn } from "react-icons/ci";
 import { IoBedOutline } from "react-icons/io5";
 import { TbBuilding } from "react-icons/tb";
+import { PiShareFatLight } from "react-icons/pi";
+
 
 
 
@@ -93,34 +95,44 @@ const RecentHouses = () => {
       <div>
         <Slider {...settings} >
            {filteredData.map((house)=>(
-            <div key={house.id} className='px-[1rem]'>
+            <div key={house.id} className='px-[1rem] shadow-[0px_2px_8.1px_0px_rgba(139,_139,_139,_0.25)]'>
               <div className='w-[250px]' >
-                <img src="./img/ev.jpg" alt="Invest Home"  className='w-[260px]'/>
+                <img src="./img/ev.jpg" alt="Invest Home"  className=''/>
               </div>
-              <div>
+              <div className='px-[0.62rem] w-[250px] rounded-[0.5rem]'>
                 <p className='font-[500] text-[var(--text-color-2)]'>{house.title}</p>
                 <div className='flex items-center gap-[0.1rem] py-[10px]'>
                   <span className='text-[var(--text-color-3)]'><CiLocationOn/></span>
                   <span className='font-[400] text-[var(--text-color-3)] text-[0.75rem]'>{house.location}</span>
                 </div>
                 <p className='font-[600] text-[var(--text-color-2)]'>$ {house.price}</p>
-                <div className='flex items-center w-[100%]'>
-                  <div className='text-[0.75rem] text-[var(--text-color-3)] flex gap-[0.75rem] w-[260px] py-[6px]'>
-                    <div className='flex items-center'>
+                <div className='text-[0.75rem] text-[var(--text-color-3)] flex justify-between w-[100%] py-[6px]'>
+                  <div className='flex justify-between w-[73%]'>
+                    <div className='flex items-center gap-[0.3rem]'>
                       <IoBedOutline />
                       <span className='font-[300] text-[var(--text-color-3)] text-[0.75rem]'>{house.beds} beds</span>
                     </div>
-                    <div className='flex items-center '>
+                    <div className='flex items-center gap-[0.3rem]'>
                       <TbBuilding />
-                      <span className='font-[300] text-[var(--text-color-3)] text-[0.75rem]'>{house.floor} floor</span>
+                      <span className='font-[300] text-[var(--text-color-3)] text-[0.75rem]'>{house.floor}</span>
                     </div>
-                    <div className='flex items-center '>
+                    <div className='flex items-center gap-[0.3rem]'>
                       <img src="./img/Square Meters.svg" alt="Invest Home" />
                       <span className='font-[300] text-[var(--text-color-3)] text-[0.75rem]'>{house.field}</span>
                     </div>
-                    {/* <span classname='text-[0.75rem] text-[var(--text-color-3)]'>{house.time}</span> */}
                   </div>
                   <div>
+                    <span classname='text-[0.75rem] text-[var(--text-color-3)]'>{house.time}</span>
+                  </div>
+                </div>
+                <hr className='w-[100%] border-[0.05rem] border-[#E5E5E5]' />
+                <div className='flex items-center justify-between p-[0.5rem]'>
+                  <div className='flex items-center gap-[0.5rem]'>
+                    <img src="./img/image.svg" alt="Invest Home" />
+                    <span className='text-[0.75rem] text-[#000] font-[500]'>Annente Blasson</span>
+                  </div>
+                  <div className='flex items-center justify-center w-[30px] h-[30px] bg-[#F3F3F3] rounded-full'>
+                    <PiShareFatLight/>
                   </div>
                 </div>
               </div>
